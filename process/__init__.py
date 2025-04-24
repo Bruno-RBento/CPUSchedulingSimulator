3ff470f48b876538782e462f5b5eab424f4a1ef4
iva449 <ivalenira@gmail.com>
2025-04-24 05:26:37 +0100

    Update __init__.py

--- process/__init__.py
+++ process/__init__.py
@@ -1,2 +1,8 @@
 # __init__.py
 
+"""
+Um ficheiro que marca a pasta process como um pacote Python,
+permitindo importar funcionalidades de dentro da pasta. Não precisa de muito código, 
+mas é essencial para que os módulos internos possam ser 
+referenciados em outras partes do projeto.
+"""
